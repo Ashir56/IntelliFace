24eb3fe0342d56636a607ef9e190e6ac6de19775
Ashir56 <ashir.rasheedx@gmail.com>
2025-05-06 12:21:14 +0500

    Added Corsheaders to my backend

--- IntelliFace/settings.py
+++ IntelliFace/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'intelliface-production.up.railway.app', '*']
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
@@ -39,17 +41,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
     'apps.users'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'IntelliFace.urls'
